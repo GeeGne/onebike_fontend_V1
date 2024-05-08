857de21874d9365db70b11b730e1891c5def5f74
Geegne <geegnebab@gmail.com>
2024-05-08 22:08:03 +0300

    Version 3.6 ImageSlider Component syntax is as clean as it can gets and it's finished for the mobile version except minor styling for the darkMode and such

--- src/Components/ImageSlider.jsx
+++ src/Components/ImageSlider.jsx
@@ -19,85 +19,69 @@ function ImageSlider () {
   const firstImage = sliderData[0].URL;
   const lastImage = sliderData[sliderData.length - 1].URL;
 
-  const vars = () => {
+  const vars = (action) => {
+    // note: in theory sliderScrollWidth should be equal to sliderScrollLeft when sliding all the way to the left,
+    // but for some reason it's acrually sliderScrollWidth is equal to sliderScrollLeft + sliderWidth.
+    
+    const getWidth = el => {
+      const computedStyle = getComputedStyle(el);
+      const width = parseFloat(computedStyle.width);
+      return width;
+    }
+
+    const scrollDirection = action ? action : 'scroll left';
     const sliderWidth = getWidth(imageSliderElement.current);
-    let sliderScrollWidth = imageSliderElement.current.scrollWidth;
+    const sliderScrollWidth = imageSliderElement.current.scrollWidth;
+    const sliderScrollLeft = imageSliderElement.current.scrollLeft;
+    // const sliderBetweenImagesWidth = sliderScrollLeft + sliderWidth;
+    const sliderBetweenImagesWidth = scrollLeft.current + (scrollDirection === 'scroll left' ? 1 : -1) * sliderWidth
+    const sliderLastImageWidth = sliderScrollWidth - sliderWidth * 2;
     const extraLength = 100;
-    let sliderScrollLeft = imageSliderElement.current.scrollLeft;
-    let sliderBetweenImagesWidth = sliderScrollLeft + sliderWidth;
-    let totalScroll = sliderScrollLeft + sliderWidth + extraLength;
+    const totalScroll = sliderScrollLeft + sliderWidth + extraLength;
     const lastIndex = sliderData.length - 1;
 
     return {
       sliderWidth,
       sliderScrollWidth,
-      extraLength,
       sliderScrollLeft,
       sliderBetweenImagesWidth,
+      sliderLastImageWidth,
+      extraLength,
       totalScroll,
       lastIndex
     }
   }
 
   useEffect(() => {
-
     action('scroll beginning');
-
     const id = setInterval(() => {
-      // const sliderWidth = getWidth(imageSliderElement.current);
-      // let sliderScrollWidth = imageSliderElement.current.scrollWidth;
-      // const extraLength = 100;
-      // let sliderScrollLeft = imageSliderElement.current.scrollLeft;
-      // let sliderBetweenImagesWidth = sliderScrollLeft + sliderWidth;
-      // let totalScroll = sliderScrollLeft + sliderWidth + extraLength;
-      // const lastIndex = sliderData.length - 1;
-      
-      if (vars().sliderScrollLeft < vars().sliderWidth) {
-        action('scroll end');
-      } 
+      // scrollLeft.current = vars().sliderScrollLeft;
 
+      vars().sliderScrollLeft < vars().sliderWidth && action('scroll end');
       if (vars().sliderScrollWidth <= vars().totalScroll) {
-        action('scroll beginning');
+        action('scroll beginning')
       }
-      
-      // sliderScrollLeft = imageSliderElement.current.scrollLeft
-      // sliderBetweenImagesWidth = sliderScrollLeft + sliderWidth;
-      // totalScroll = sliderScrollLeft + sliderWidth + extraLength;
-      scroll(imageSliderElement.current, vars().sliderBetweenImagesWidth, 'smooth')
-      setCurrentImage(old => vars().lastIndex === old ? 0 : old + 1);
-      // setCurrentImage(old => old + 1);
+      scrollLeft.current = vars().sliderScrollLeft
+
+      action('scroll left');
     }, 4000)
 
     return () => clearInterval(id);
   }, []);
 
-  useEffect(() => {
-    console.log(currentImage);
-  }, [currentImage])
-
   const handleStart = e => {
-    // note: in theory sliderScrollWidth should be equal to sliderScrollLeft when sliding all the way to the left,
-    // but for some reason it's acrually sliderScrollWidth is equal to sliderScrollLeft + sliderWidth.
-
-    // const sliderWidth = getWidth(e.currentTarget);
-    // const sliderScrollWidth = e.currentTarget.scrollWidth;
-    // const sliderScrollLeft = e.currentTarget.scrollLeft;
-    // const extraLength = 100;
-    // const totalScroll = sliderScrollLeft + sliderWidth + extraLength
-
     vars().sliderScrollWidth <= vars().totalScroll && action('scroll beginning');
     vars().sliderScrollLeft < vars().sliderWidth && action('scroll end');
 
     initialX.current = e.touches[0].clientX;
-    // scrollLeft.current = vars().sliderScrollLeft = e.currentTarget.scrollLeft;
-    scrollLeft.current = vars().sliderScrollLeft = e.currentTarget.scrollLeft;
+    scrollLeft.current = vars().sliderScrollLeft;
   }
 
   const handleMove = e => {
     currentX.current = e.touches[0].clientX;
     amountX.current = initialX.current - currentX.current;
     const totalAmount =  scrollLeft.current + amountX.current;
-    scroll(e.currentTarget, totalAmount, 'instant');
+    action('move', totalAmount);
   }
 
   const handleEnd = e => {
@@ -107,65 +91,56 @@ function ImageSlider () {
     } else if (amountX.current < -1 * activateLength) {
       action('scroll right');
     } else {
-      scroll(e.currentTarget, scrollLeft.current, 'smooth');
+      action('return');
     }
 
     amountX.current = 0;
   }
 
-  function action (action) {
-    const sliderWidth = getWidth(imageSliderElement.current);
-    const sliderScrollWidth = imageSliderElement.current.scrollWidth;
-    const sliderLastImageWidth = sliderScrollWidth - sliderWidth * 2;
-    const sliderBetweenImagesWidth = scrollLeft.current + (action === 'scroll left' ? 1 : -1) * sliderWidth
-    const lastIndex = sliderData.length - 1;
+  function action (action, totalAmount) {
+
+    const scroll = (e, left, behavior) => e.scrollTo({left,behavior});
 
     const scrollToBeginning = () => {
-      scroll(imageSliderElement.current, sliderWidth, 'instant');
+      scroll(imageSliderElement.current, vars().sliderWidth, 'instant');
       setCurrentImage(0);
     }
 
     const scrollToEnd = () => {
-      scroll(imageSliderElement.current, sliderLastImageWidth, 'instant');
-      setCurrentImage(lastIndex);
+      scroll(imageSliderElement.current, vars().sliderLastImageWidth, 'instant');
+      setCurrentImage(vars().lastIndex);
     }
 
-    const scrollToLeft = () => {
-      scroll(imageSliderElement.current, sliderBetweenImagesWidth, 'smooth');    
-      setCurrentImage(oldNum => oldNum + 1);
+    const scrollToLeft = () => {  
+      scroll(imageSliderElement.current, vars('scroll left').sliderBetweenImagesWidth, 'smooth');    
+      setCurrentImage(oldNum => vars().lastIndex === oldNum ? 0 : oldNum + 1);
     }
 
-    const scrollToRight = () => {
-      scroll(imageSliderElement.current, sliderBetweenImagesWidth, 'smooth');    
-      setCurrentImage(oldNum => oldNum - 1);
+    const scrollToRight = () => { 
+      scroll(imageSliderElement.current, vars('scroll right').sliderBetweenImagesWidth, 'smooth');    
+      setCurrentImage(oldNum => oldNum  === 0 ? vars().lastIndex : oldNum - 1);
+    }
+
+    const returnBack = () => { 
+      scroll(imageSliderElement.current, scrollLeft.current, 'smooth');
+    
+    }
+    const move = totalAmount => { 
+      scroll(imageSliderElement.current, totalAmount, 'instant');
     }
 
     action === 'scroll beginning' && scrollToBeginning();
     action === 'scroll end' && scrollToEnd();
     action === 'scroll left' && scrollToLeft();
     action === 'scroll right' && scrollToRight();
+    action === 'return' && returnBack();
+    action === 'move' && move(totalAmount);
   }
-
-  const scroll = (e, left, behavior) => e.scrollTo({left,behavior});
-
-  const getWidth = el => {
-    const computedStyle = getComputedStyle(el);
-    const width = parseFloat(computedStyle.width);
-    return width;
-  }
-
 
   return (
     <>
       <section className='imageSlider-container'>
-        <ul className='imageSlider-container__img-holder' onTouchStart={handleStart} 
-          // onMouseEnter={handleStart} 
-          onTouchMove={handleMove} 
-          // onMouseDown={handleMove} 
-          onTouchEnd={handleEnd} 
-          // onMouseUp={handleEnd}
-          ref={imageSliderElement}
-        >
+        <ul className='imageSlider-container__img-holder' onTouchStart={handleStart}  onTouchMove={handleMove} onTouchEnd={handleEnd} ref={imageSliderElement}>
           <li className='imageSlider-container__img-holder__imges'><img src={lastImage}/></li>
           {sliderData.map(data =>
           <li className='imageSlider-container__img-holder__imges' key={data.id}>
