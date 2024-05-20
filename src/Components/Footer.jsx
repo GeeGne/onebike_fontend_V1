fce08d60862cbec3cb024eedad27487620a863df
Geegne <geegnebab@gmail.com>
2024-05-20 05:48:12 +0300

    Version 5.3 Footer component is now supports arabic language and optimized for all screen sizes, add new component called NeedHelp

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -21,32 +21,44 @@ import mailIconDarkMode from '/src/assets/Img/Icons/mail_darkMode.svg';
 
 function Footer ({darkMode, lan}) {
 
+  const facebookURL = "https://www.facebook.com/profile.php?id=61555487381717";
+  const instagramURL = "https://www.instagram.com/onebike2024?fbclid=IwZXh0bgNhZW0CMTAAAR0j3tRBpSGxAkPztNBkI-KKoBI454wtiODT8gyzjrY2B6jtQhzTFDkpvuI_aem_AZvvFyQoDJ0_EqQr36CwzmanbLAnK_nFqsXLZollXvn2m7LBaFueZpOgtR9S0sRbuLf_CynuMk7xMjEVi3Capb1V";
+  const whatsAppURL = "https://chat.whatsapp.com/BanGDxwaSLgKMBWN6eDVzq";
 
   return (
     <div className="footer-container">
       <div className="footer-container__footer-upper-grid">
+        <section className="footer-container__footer-upper-grid__section-subscribe">
+          <h2 className="footer-container__footer-upper-grid__section-subscribe__title">{lan === 'en' ? 'Subscribe to our newsletter' : 'اشترك في نشرتنا'}</h2>
+          <ul className="footer-container__footer-upper-grid__section-subscribe__list">
+            <li className="footer-container__footer-upper-grid__section-subscribe__list__item">
+              <input className="footer-container__footer-upper-grid__section-subscribe__list__item__input" placeholder={lan === 'en' ? 'Email address' : 'عنوان البريد الاكتروني'} />
+              <button className="footer-container__footer-upper-grid__section-subscribe__list__item__button">{lan === 'en' ? 'Subscribe': 'اشترك'}</button>
+            </li>
+          </ul>
+        </section>
         <section className="footer-container__footer-upper-grid__section-quick-links">
-          <h2 className="footer-container__footer-upper-grid__section-quick-links__title">Quick Links</h2>
+          <h2 className="footer-container__footer-upper-grid__section-quick-links__title">{lan === 'en' ? 'Quick Links' : 'روابط سريعه'}</h2>
           <ul className="footer-container__footer-upper-grid__section-quick-links__list">
             <li className="footer-container__footer-upper-grid__section-quick-links__list__item">
-              <Link className="footer-container__footer-upper-grid__section-quick-links__list__item__link">About us</Link>
+            <Link className="footer-container__footer-upper-grid__section-quick-links__list__item__link">{lan === 'en' ? 'About us' : 'عنّا'}</Link>
             </li>
             <li className="footer-container__footer-upper-grid__section-quick-links__list__item">
-              <Link className="footer-container__footer-upper-grid__section-quick-links__list__item__link">Privacty Policy</Link>
+              <Link className="footer-container__footer-upper-grid__section-quick-links__list__item__link">{lan === 'en' ? 'Privacty Policy' : 'سياسه الخصوصيه'}</Link>
             </li>
             <li className="footer-container__footer-upper-grid__section-quick-links__list__item">
-              <Link className="footer-container__footer-upper-grid__section-quick-links__list__item__link">Back to Home</Link>
+              <Link className="footer-container__footer-upper-grid__section-quick-links__list__item__link" to="/">{lan === 'en' ? 'Back to Home' : 'العوده الى الرئيسيه'}</Link>
             </li>
             <li className="footer-container__footer-upper-grid__section-quick-links__list__item">
-              <Link className="footer-container__footer-upper-grid__section-quick-links__list__item__link">Shipping process</Link>
+              <Link className="footer-container__footer-upper-grid__section-quick-links__list__item__link">{lan === 'en' ? 'Shipping process' : 'عمليه الشحن'}</Link>
             </li>
             <li className="footer-container__footer-upper-grid__section-quick-links__list__item">
-              <Link className="footer-container__footer-upper-grid__section-quick-links__list__item__link">Commonl Asked Questions</Link>
+              <Link className="footer-container__footer-upper-grid__section-quick-links__list__item__link">{lan === 'en' ? 'Commonly Asked Questions' : 'الأسئله الشائعه'}</Link>
             </li>
           </ul>
         </section>
         <section className="footer-container__footer-upper-grid__section-contact-us">
-          <h2 className="footer-container__footer-upper-grid__section-contact-us__title">Contact Us</h2>
+          <h2 className="footer-container__footer-upper-grid__section-contact-us__title">{lan === 'en' ? 'Contact Us' : 'تواصل معنا'}</h2>
           <ul className="footer-container__footer-upper-grid__section-contact-us__list">
             <li className="footer-container__footer-upper-grid__section-contact-us__list__item">
               <img className="footer-container__footer-upper-grid__section-contact-us__list__item__img" src={darkMode ? callIconDarkMode : callIcon}/>
@@ -58,21 +70,20 @@ function Footer ({darkMode, lan}) {
             </li>
             <li className="footer-container__footer-upper-grid__section-contact-us__list__item">
               <img className="footer-container__footer-upper-grid__section-contact-us__list__item__img" src={darkMode ? whatsappIconDarkMode : whatsappIcon}/>
-              <Link className="footer-container__footer-upper-grid__section-contact-us__list__item__link">Chat with us</Link>
+            <Link className="footer-container__footer-upper-grid__section-contact-us__list__item__link" to={whatsAppURL} target="_blank">{lan === 'en' ? 'Chat with us' : 'تحدث معنا'}</Link>
             </li>
           </ul>
         </section>
-   
       </div>
 
       <div className="footer-container__footer-lower">
         <section className="footer-container__footer-lower__media-section">
-        <a><img className="footer-container__footer-lower__media-section__media-icon" src={darkMode ? facebookIcon : facebookIconDarkMode}/></a>
-        <a><img className="footer-container__footer-lower__media-section__media-icon" src={darkMode ? whatsappIcon : whatsappIconDarkMode}/></a>
-        <a><img className="footer-container__footer-lower__media-section__media-icon" src={darkMode ? instagramIcon : instagramIconDarkMode}/></a>
+        <a href={facebookURL} target="_blank"><img className="footer-container__footer-lower__media-section__media-icon" src={darkMode ? facebookIcon : facebookIconDarkMode}/></a>
+        <a href={whatsAppURL} target="_blank"><img className="footer-container__footer-lower__media-section__media-icon" src={darkMode ? whatsappIcon : whatsappIconDarkMode}/></a>
+        <a href={instagramURL} target="_blank"><img className="footer-container__footer-lower__media-section__media-icon" src={darkMode ? instagramIcon : instagramIconDarkMode}/></a>
         </section>
         <section className="footer-container__footer-lower__organism-rights">
-          <h3 className="footer-container__footer-lower__organism-rights__h3">Syria © 2024 ONE BIKE all rights reseved</h3>
+          <h3 className="footer-container__footer-lower__organism-rights__h3">{lan === 'en' ? 'Syria © 2024 ONE BIKE all rights reseved' : 'سوريا © 2024 ون بايك جميع الحقوق محفوظة'}</h3>
         </section>
       </div>
     </div>
---
+++ src/Components/NeedHelp.jsx
@@ -0,0 +1,15 @@
+// HOOKS
+import React, {useState, useEffect, useRef} from 'react';
+
+// SCSS
+import '/src/Styles/Components/NeedHelp.scss';
+
+function NeedHelp () {
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default NeedHelp;
