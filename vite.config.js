32263dbafddcc3c2f7c1a4d2264b120a19ae65cf
Geegne <geegnebab@gmail.com>
2024-08-15 04:55:39 +0300

    --ammend

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        '/src/firebase/authSignUp.js',
-        '/src/firebase/fireStore'
-      ]
-    }
-  }
+  // build: {
+  //   rollupOptions: {
+  //     external: [
+  //       '/src/firebase/authSignUp.js',
+  //       '/src/firebase/fireStore'
+  //     ]
+  //   }
+  // }
 })
