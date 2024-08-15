a29101da6d76615d0d92096238e2d78c0c00856f
Geegne <geegnebab@gmail.com>
2024-08-15 04:32:07 +0300

    --ammend

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        '/src/firebase/authSignUp.js', // Add your problematic module here
+        // Add other external modules if needed
+      ]
+    }
+  }
 })
