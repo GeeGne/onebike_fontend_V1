9453d8e36d5b32d42ff9f7ce207d6afc51edf162
Geegne <geegnebab@gmail.com>
2024-08-15 15:14:33 +0300

    --ammend

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // HOOKS
 import React, {useEffect, useState, useRef, Suspense} from 'react';
-import {HashRouter as Router, Routes, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import {HelmetProvider} from 'react-helmet-async';
 
 //  STYLES
